e3ee01bea962f5938d0b2223d00ca0e75eb5502f
Programador jotilson <domingosnzingamiala12@gmail.com>
2026-02-02 23:10:26 +0100

    Paginas
    Adicionando novas paginas

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -227,8 +227,16 @@ img.src=`https://openweathermap.org/img/wn/${icon}@2x.png`
  }
  Clima()
 
-
-
-
 })
- 
+ const trocarMenu=document.getElementById("menu")
+ const cp=document.getElementById("Capa")
+ trocarMenu.addEventListener("click",function(){
+    if(trocarMenu.innerHTML=='<i class="fa-solid fa-bars fa-2xl" style="color: #63E6BE;"></i>'){
+ trocarMenu.innerHTML='<i class="fa-solid fa-xmark fa-2xl" style="color: #63E6BE;"></i>'
+  cp.style.display='block'
+    }else{
+        trocarMenu.innerHTML='<i class="fa-solid fa-bars fa-2xl" style="color: #63E6BE;"></i>'
+         cp.style.display='none'
+    }
+   
+ })
